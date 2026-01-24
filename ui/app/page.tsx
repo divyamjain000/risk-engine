d18b4c4aa4d3c3c983f5257a293fdf2733e92187
Divyam Jain <jaindivyam111@gmail.com>
2026-01-25 01:15:23 +0530

    infra: dockerize api & ui with nginx, add gitignore cleanup

---
+++ ui/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Hello World 👋</h1>
+      <p>If you see this, the UI is working.</p>
+    </main>
+  );
+}
